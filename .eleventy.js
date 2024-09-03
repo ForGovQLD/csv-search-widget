1e8996ea3ad66668e7c77aa4dbb009446b1bf7b8
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2024-09-03 16:33:42 +1000

    Add eleventy-navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin)
+
   // eleventyConfig.addPassthroughCopy("layout.css")
   eleventyConfig.addPassthroughCopy("css/csv-search-widget.css")
   eleventyConfig.addPassthroughCopy("css/layout.css")
